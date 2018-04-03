1a8104a4b41c6e54f327aab4abf754d372ab5880
Merhy Daychoum <merhydaychoum@gmail.com>
2018-04-03 13:02:21 -0300

    First commit

---
+++ app/src/main/java/br/usjt/meuprimeiroapp/MainActivity.java
@@ -0,0 +1,33 @@
+package br.usjt.meuprimeiroapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+/**
+ * @autor Merhy Omar Daychoum
+ * RA: 816110728
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public final static String EXTRA_MESSAGE =
+            "br.usjt.meuprimeiroapp.MESSAGE";
+    //será chamado quando o usuário clicar em Enviar
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
